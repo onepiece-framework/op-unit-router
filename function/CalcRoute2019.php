200b3876cccdffa7f9f3637a0754c9976666a94e
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2022-10-12 18:31:43 +0900

    New: function > CalcRoute2019.php

---
+++ function/CalcRoute2019.php
@@ -0,0 +1,24 @@
+<?php
+/** op-unit-router:/function/CalcRoute2019.php
+ *
+ * @creation  2019-02-23 Separate from NewWorld.
+ * @version   1.0
+ * @package   unit-router
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @created   2018-04-13  OP\UNIT\NEWWORLD
+ * @updated   2019-02-23  OP\UNIT
+ */
+namespace OP\UNIT;
+
+/** Calculate route.
+ *
+ */
+function CalcRoute2019()
+{
+
+}
