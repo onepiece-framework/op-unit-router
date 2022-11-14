050f7c72c2e132cda56f81cbc1ee0aa4b92b5910
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2022-11-14 19:51:42 +0900

    CI: ci.sh

---
+++ ci.sh
@@ -0,0 +1,13 @@
+
+ ## op-unit-router:/ci.sh
+ #
+ # Call from git pre-push
+ #
+ # @created   2022-11-12
+ # @version   1.0
+ # @package   op-unit-router
+ # @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ # @copyright Tomoaki Nagahara All right reserved.
+
+php ../../../ci.php display=0
+
