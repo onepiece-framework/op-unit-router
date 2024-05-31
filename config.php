f339f332e70dd21d0be5cdc90b619bedc36c15ea
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-05-31 21:22:04 +0900

    Doc: config.php | PHPDoc

--- config.php
+++ config.php
@@ -1,2 +1,10 @@
 <?php
+/** op-unit-router:/config.php
+ *
+ * @created    2024-05-31
+ * @version    1.0
+ * @package    op-unit-router
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
 return [];
