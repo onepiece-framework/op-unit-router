94e47678adfa8ad76507a9191955d695c3913065
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-06 21:06:34 +0900

    Doc: index.php | PHPDoc

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * @created   2019-02-23
  * @version   1.0
  * @package   op-unit-router
- * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @author    Tomoaki Nagahara
  * @copyright Tomoaki Nagahara All right reserved.
  */
 //	...
